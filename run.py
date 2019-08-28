5ca9a921c27033961a827f4ace35687277812115
Luba <luba@penguin.tech>
2019-08-28 12:18:25 -0500

    Added script for extracting xml ID's into .csv files

---
+++ run.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import os
+import shutil
+import csv
+
+
+
+current_dir = os.getcwd()
+
+source_folder = os.path.join(current_dir, "source")
+fail_folder = os.path.join(current_dir, "fail")
+pass_folder = os.path.join(current_dir, "pass")
+results_folder = os.path.join(current_dir, "results")
+
+
+
+def main():
+
+	create_folders()
+	create_csv()
+	process_files()
+
+
+# Create pass/fail/results folders if they don't exist
+def create_folders():
+	if not (os.path.isdir(fail_folder)):
+		os.makedirs(fail_folder)
+		
+	if  not (os.path.isdir(pass_folder)):
+		os.makedirs(pass_folder)
+
+	if  not (os.path.isdir(results_folder)):
+		os.makedirs(results_folder)
+
+
+
+	
+# Create csv files from xml file names
+def create_csv():
+
+	input_folder = os.listdir(source_folder)
+	os.chdir(results_folder)
+
+	csv_file_count = 1
+	count = 0
+	ids_per_csv = 10
+
+	f = open(str(csv_file_count) + '.csv','w')
+
+	for file in input_folder:
+		if count < ids_per_csv:
+			f.write(file+'\n')
+			count = count +1
+		if count == ids_per_csv:
+			f.close()
+			csv_file_count += 1
+			f = open(str(csv_file_count) + '.csv','w')
+			count = 0
+
+	f.close()
+
+
+
+# Logic for payloads that pass or fail
+# TODO: 
+def process_files():
+
+	os.chdir(source_folder)
+	folder = os.listdir(source_folder)
+	
+
+	for file in folder:
+		
+		if (file.startswith("1")):
+			shutil.move(file, fail_folder)
+
+		elif (file.startswith("2")):
+			shutil.move(file, pass_folder)
+
+main()
